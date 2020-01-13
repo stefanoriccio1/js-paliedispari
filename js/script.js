c7b3421822f300513db4728079230b2a7f0c2c77
Stefano Riccio <riccio.stefano0@gmail.com>
2020-01-13 15:32:35 +0100

    up

--- js/script.js
+++ js/script.js
@@ -9,7 +9,12 @@ var checkParola = [];
 checkParola.push(parola);
 console.log(checkParola);
 
-for (i=0; i < parola.length; i++){
-console.log(parola[i]);
-if (parola[i])
-}
+var sliced = parola.slice(0,2);
+console.log(sliced);
+var sliced2 =parola.slice(4,3);
+console.log(sliced2);
+
+// for (i=0; i < parola.length; i++){
+// console.log(parola[i]);
+// if (parola[i])
+// }
