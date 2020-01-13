0c599224d1d8f288ae8605aaefc3088208ec022a
Stefano Riccio <riccio.stefano0@gmail.com>
2020-01-13 15:29:15 +0100

    up

--- js/script.js
+++ js/script.js
@@ -3,3 +3,13 @@
 // Scrivi una funzione per capire se la parola è palindroma
 
 var parola = prompt ('inserisci una parola');
+
+var checkParola = [];
+
+checkParola.push(parola);
+console.log(checkParola);
+
+for (i=0; i < parola.length; i++){
+console.log(parola[i]);
+if (parola[i])
+}
